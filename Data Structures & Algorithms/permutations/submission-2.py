05e532899fc35107cc89a682044526e56d7f0e40
Kushagra-Agnihotri <vagnikush@gmail.com>
2026-07-13 15:25:53 +0530

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def rec(i, current):
+            nonlocal res
+            if i== len(nums):
+                res.append(current.copy())
+
+                return 
+            for j in range(len(current)+1):
+                current.insert(j, nums[i])
+                rec(i+1, current)
+                current.pop(j)
+        rec(1,[nums[0]])
+        return res
+
