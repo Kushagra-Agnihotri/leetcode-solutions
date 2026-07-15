ac8d98e000b788a30ddd6cb55d8ee3805a648716
Kushagra-Agnihotri <vagnikush@gmail.com>
2026-07-15 18:50:45 +0530

    Add: n-queens - submission-2

---
+++ Data Structures & Algorithms/n-queens/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        board = [["."]*n for _ in range(n)]
+
+        
+        def check(i,j, board):
+            row = board[i]
+            col = [board[x][j] for x in range(n)]
+            # Upper-left diagonal
+            ldia = [board[i-x-1][j-x-1] for x in range(min(i,j))]
+            # Upper-right diagonal
+            rdia = [board[i-x-1][j+x+1] for x in range(min(i, n-1-j))]
+            
+            if "Q" in col+row+ldia+rdia:
+                return False
+            return True
+
+        def dfs(k):
+            nonlocal board
+            
+            if k==n:
+                res.append(["".join(row) for row in board])
+                return
+            for j in range(n):
+                if check(k,j, board):           
+
+                    board[k][j] = "Q"
+                    dfs(k+1)
+                    board[k][j] = "."
+        dfs(0)
+        return res
