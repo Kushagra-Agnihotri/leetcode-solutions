5956baf0ba9e7f09f718f38c001b677f5c06ebe6
Kushagra-Agnihotri <vagnikush@gmail.com>
2026-08-08 08:48:51 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.py
@@ -0,0 +1,34 @@
+class DSU:
+    def __init__(self, n) -> None:
+        self.par = list(range(n+1))
+        self.rank = [1] * (n+1)
+    def find(self, n):
+        if n !=  self.par[n]:
+            self.par[n] =  self.find(self.par[n])
+        return self.par[n]
+    def union(self, n1 , n2):
+        p1, p2 = self.find(n1), self.find(n2)
+
+        if p1 == p2: return False
+
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+        elif self.rank[p2] > self.rank[p1]:
+            self.par[p1] = p2
+        else:
+            self.par[p1] = p2
+            self.rank[p2] +=1
+        return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
+        n = len(edges)
+        res = 0
+        dsu = DSU(n)
+        for u, v in edges:
+            if dsu.union(u, v):
+                res +=1
+            else:
+                return[u, v]
+        
