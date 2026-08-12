6430abfade4fba8b61a981565e51be1c90daec9e
Kushagra-Agnihotri <vagnikush@gmail.com>
2026-08-12 12:46:08 +0530

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = []
+        h= {nums[i]: i for i in range(len(nums))}
+        print(h)
+        for i in range(len(nums)):
+            t = target - nums[i]
+            if t in h and h[t] != i:
+                res = [i, h[t]]
+                break
+
+        return res
